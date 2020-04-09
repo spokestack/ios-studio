58b7a28af72254e9c1bbaeb8220daf1db2013054
Daniel Tyreus <tyreus@synclab.com>
2020-04-09 07:24:45 -0700

    Initial Commit

---
+++ Spokestack Studio iOSTests/Spokestack_Studio_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  Spokestack_Studio_iOSTests.swift
+//  Spokestack Studio iOSTests
+//
+//  Created by Daniel Tyreus on 4/9/20.
+//  Copyright © 2020 Spokestack. All rights reserved.
+//
+
+import XCTest
+@testable import Spokestack_Studio_iOS
+
+class Spokestack_Studio_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
